34188b719a97a4e648a56995c876a374fe897e8d
nickborti <saaggy18@gmail.com>
2022-06-30 00:45:33 +0530

    aws ses test email on registration

--- server/contollers/auth.js
+++ server/contollers/auth.js
@@ -1,5 +1,44 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: process.env.AWS_REGION,
+});
+
+const ses = new AWS.SES({ apiVersion: '2010-12-01' });
+
 exports.register = (req, res) => {
-	res.json({
-		status: 'OK',
-	});
+	const { name, email, password } = req.body;
+	const params = {
+		Source: process.env.EMAIL_FROM,
+		Destination: {
+			ToAddresses: [email],
+		},
+		ReplyToAddresses: [process.env.EMAIL_TO],
+		Message: {
+			Body: {
+				Html: {
+					Charset: 'UTF-8',
+					Data: `<html><body><h1>Hello ${name}</h1><p style="color:red;">Test Email</p></body></html>`,
+				},
+			},
+			Subject: {
+				Charset: 'UTF-8',
+				Data: 'Complete your registration',
+			},
+		},
+	};
+
+	const sendEmailOnRegister = ses.sendEmail(params).promise();
+
+	sendEmailOnRegister
+		.then((data) => {
+			console.log('Email submitted to SES ', data);
+			res.send('Email sent');
+		})
+		.catch((error) => {
+			console.log('ses email on register error ', error);
+			res.send('Email failed');
+		});
 };
